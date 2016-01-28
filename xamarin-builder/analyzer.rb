8e4194bc85db7fe8b29128fc50d397f6db2c6c27
birmacher <birmacher@gmail.com>
2016-01-28 18:31:01 +0100

    xamarin-builder updated

--- xamarin-builder/analyzer.rb
+++ xamarin-builder/analyzer.rb
@@ -111,14 +111,6 @@ class Analyzer
           sign_android = project[:configs][project_configuration][:sign_android]
 
           build_commands << [
-              MDTOOL_PATH,
-              'build',
-              "\"-c:#{project_configuration}\"",
-              @solution[:path],
-              "-p:#{project[:name]}"
-          ].join(' ')
-
-          build_commands << [
               'xbuild',
               sign_android ? '/t:SignAndroidPackage' : '/t:PackageForAndroid',
               "/p:Configuration=#{project_configuration.split('|').first}",
@@ -152,6 +144,7 @@ class Analyzer
           'build',
           "\"-c:#{project_configuration}\"",
           @solution[:path],
+          "-p:#{project[:name]}"
       ].join(' ')
     end
 
